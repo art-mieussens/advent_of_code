69a6f4fbe46f50ffc33ea7fb8a84b2bc8dc0c979
Art Mieussens <artmieu@gmail.com>
2019-05-24 11:29:54 +0100

    day6 ... more work

--- day6.rb
+++ day6.rb
@@ -1,3 +1,4 @@
+# Initialize array
 arr = []
 for i in 0..999 do
   arr[i] = []
@@ -5,6 +6,8 @@ for i in 0..999 do
     arr[i][j] = false
   end
 end
+
+# Methods to modify a range
 
 def turn_on(x1,y1,x2,y2)
   for i in (x1..x2) do
@@ -30,19 +33,43 @@ def toggle(x1,y1,x2,y2)
   end
 end
 
+# Initialize range parameters
+x1, x2, y1, y2 = 0, 0, 0, 0
+
+# Get coordinates from comma separated strings on ints
+def getcoords(command1, command2)
+  x1, y1 = command1.split(',').map{ |c| c.to_i }
+  x2, y2 = command2.split(',').map{ |c| c.to_i }
+end
+
 file = File.open("day6_input")
 
 file.each_line do |l|
+  # Parse the instructions and call modifier methods
   command = l.split(' ')
   case command[0]
   when 'turn'
     case command[1]
     when  'on'
-      puts "turn on #{command[2]} through #{command[4]}" 
+      getcoords(command[3], command[5])
+      turn_on(x1,y1,x2,y2)
     when 'off'
-      puts "turn off #{command[2]} through #{command[4]}"
+      getcoords(command[3], command[5])
+      turn_off(x1,y1,x2,y2)
     end
   when 'toggle'
-    puts "toggle #{command[1]} through #{command[3]}"
+    getcoords(command[2], command[4])
+    toggle(x1,y1,x2,y2)
   end 
 end
+
+# Count lights on
+counter = 0
+for i in 0..999 do
+  arr[i] = []
+  for j in 0..999 do
+    counter += 1 if arr[i][j] = true
+  end
+end
+
+puts counter
