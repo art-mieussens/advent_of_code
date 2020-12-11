8015811692d109885f8b17c519c0ac5a4e5e34fd
Art Mieussens <artmieu@gmail.com>
2020-12-11 10:52:34 +0000

    Wrap in a class

--- 2020/day8/day8.rb
+++ 2020/day8/day8.rb
@@ -1,39 +1,43 @@
-program = []
-File.open("input.txt").each_line do |line|
-    program << line
-end
+class Program
 
-index_after_last = program.length + 1
-
-def run_program
-    accumulator = 0
-    index = 0
-
-    instructions_ran = {}
-
-    until instructions_ran[index] do
-        puts "----"
-        puts "index: #{index}"
-        puts "instruction: #{program[index]}"
-        instructions_ran[index] = true
-        par = program[index].match(/^....((?:\+|-)\d+)/)[1].to_i
-        case program[index].match(/^(...)/)[1]
-        when "nop"
-            index += 1
-        when "acc"
-            accumulator += par
-            index += 1
-        when "jmp"
-            index += par
+    def initialize(input)
+        @program = []
+        File.open(input).each_line do |line|
+            @program << line
+        end
+        @accumulator = 0
+        @index = 0
+    end
+        
+    def run
+        instructions_ran = {}
+        until instructions_ran[@index] do
+            puts "----"
+            puts "index: #{@index}"
+            puts "instruction: #{@program[@index]}"
+            instructions_ran[@index] = true
+            par = @program[@index].match(/^....((?:\+|-)\d+)/)[1].to_i
+            case @program[@index].match(/^(...)/)[1]
+            when "nop"
+                @index += 1
+            when "acc"
+                @accumulator += par
+                @index += 1
+            when "jmp"
+                @index += par
+            end
+        end
+        puts "===="
+        puts "index: #{@index}"
+        puts "accumulator: #{@accumulator}"
+        puts "===="
+        if @index == @program.length + 1
+            return "program ended"
+        else
+            return "program halted"
         end
     end
-    puts "===="
-    puts "index: #{index}"
-    puts "accumulator: #{accumulator}"
-    puts "===="
-    if index == index_after_last
-        return "program ended"
-    else
-        return "program halted"
-    end
 end
+
+p = Program.new("input.txt")
+p.run
